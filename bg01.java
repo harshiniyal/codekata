bf24b4f09f5b568dad36a7edf6240f6c4a3f96e0
harshiniyal <48345169+harshiniyal@users.noreply.github.com>
2019-03-08 11:54:28 +0530

    Create bg01.java

---
+++ bg01.java
@@ -0,0 +1,43 @@
+    import java.util.Scanner;
+
+   class Postive_Negative 
+
+    {
+
+        public static void main(String[] args) 
+
+        {
+
+            int n;
+
+            Scanner s = new Scanner(System.in);
+
+          
+            n = s.nextInt();
+
+            if(n > 0)
+
+            {
+                System.out.println("Positive");
+
+            }
+
+            else if(n < 0)
+
+            {
+
+                System.out.println("Negative");
+
+            }
+
+            else
+
+            {
+
+                System.out.println("Zero");
+
+            }
+
+        }
+
+    }
